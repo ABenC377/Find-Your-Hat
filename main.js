2df3231218081fc8e98358e4bab7b97668ba1ee7
ABenC377 <84676155+ABenC377@users.noreply.github.com>
2022-01-15 13:53:34 +0000

    makes game start playing on load

--- main.js
+++ main.js
@@ -9,19 +9,21 @@ class Field {
   constructor (field) {
     this._field = field
     this._playerLocation = [0, 0]
+    this._hatLocation = [0, 0]
   }
 
   print () {
-    for (let i = 0; i < this._field.len(); i++) {
-      for (let j = 0; j < this._field[i].len(); j++) {
-        console.log(this._field[i][j])
+    for (let i = 0; i < this._field.length; i++) {
+      let rowString = ''
+      for (let j = 0; j < this._field[i].length; j++) {
+        rowString += this._field[i][j]
       }
-      console.log('\n')
+      console.log(rowString)
     }
   }
 
   win () {
-    if (this._field[this._playerLocation[0]][this._playerLocation[1]] === hat) {
+    if (this._playerLocation === this._hatLocation) {
       return true
     } else {
       return false
@@ -31,7 +33,7 @@ class Field {
   lose () {
     if (this._field[this._playerLocation[0]][this._playerLocation[1]] === hole) {
       return true
-    } else if ((this._playerLocation[0] < 0) || (this._playerLocation[0] >= this._field.len()) || (this._playerLocation[1] < 0) || (this._playerLocation[1] >= this._field[0].len())) {
+    } else if ((this._playerLocation[0] < 0) || (this._playerLocation[0] >= this._field.length) || (this._playerLocation[1] < 0) || (this._playerLocation[1] >= this._field[0].length)) {
       return true
     } else {
       return false
@@ -53,8 +55,8 @@ class Field {
       console.log('please provide a valid direction (e.g., "u", "r", "d" or "l")')
       return true
     }
-    this._field[this._playerLocation[0]][this._playerLocation[1]] = pathCharacter
     this._playerLocation = newPlayerLocation
+    this._field[this._playerLocation[0]][this._playerLocation[1]] = pathCharacter
     if (this.lose()) {
       console.log('You Lose')
       return false
@@ -66,45 +68,71 @@ class Field {
   }
 
   playGame () {
+    this.print()
     while (this.getDirection()) {
       this.print()
     }
   }
 
   static generateField (height, width, density) {
-    if ((height < 1) || (height > 50) || (typeof (height) !== 'number')) {
-      console.log('height must be a number between 1 and 50')
-      return Field.generateField()
-    }
-    if ((width < 1) || (width > 50) || (typeof (width) !== 'number')) {
-      console.log('width must be a number between 1 and 50')
-      return Field.generateField()
-    }
-    if ((density < 0) || (density > 1) || (typeof (density) !== 'number')) {
-      console.log('density must be a value between 0 and 1')
-      return Field.generateField()
-    }
     const newField = []
     let hatCounter = Math.floor((Math.random() * ((height * width) - 1)) + 1)
     for (let i = 0; i < height; i++) {
       const newRow = []
       for (let j = 0; j < width; j++) {
         if ((i === 0) && (j === 0)) {
-          newRow.append(fieldCharacter)
+          newRow.push(pathCharacter)
         } else if (hatCounter === 0) {
-          newRow.append(hat)
+          newRow.push(hat)
+          this._hatLocation = [i, j]
         } else {
-          const randomHoleGenerator = Math.Random()
+          const randomHoleGenerator = Math.random()
           if (randomHoleGenerator > density) {
-            newRow.append(fieldCharacter)
+            newRow.push(fieldCharacter)
           } else {
-            newRow.append(hole)
+            newRow.push(hole)
           }
         }
         hatCounter -= 1
       }
-      newField.append(newRow)
+      newField.push(newRow)
     }
     return new Field(newField)
   }
 }
+
+function getHeight () {
+  let height = Number(prompt('What height would you like your field to be? '))
+  if ((height < 1) || (height > 50) || (isNaN(height))) {
+    console.log('Height must be a number between 1 and 50')
+    height = getHeight()
+  }
+  return height
+}
+
+const height = getHeight()
+
+function getWidth () {
+  let width = Number(prompt('What width would you like your field to be? '))
+  if ((width < 1) || (width > 50) || (isNaN(width))) {
+    console.log('Width must be a number between 1 and 50')
+    width = getWidth()
+  }
+  return width
+}
+
+const width = getWidth()
+
+function getDensity () {
+  let density = Number(prompt('How dense would you like your field to be with holes? '))
+  if ((density < 0) || (density > 1) || (isNaN(density))) {
+    console.log('Density must be a number between 0 and 1')
+    density = getDensity()
+  }
+  return density
+}
+
+const density = getDensity()
+
+const gameField = Field.generateField(height, width, density)
+gameField.playGame()
